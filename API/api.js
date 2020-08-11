393dfa485e70ff178312d527927469e5a2eac311
Tijana <tijana.ilic87@protonmail.com>
2020-08-12 01:24:29 +0200

    Create POST /orders route and createOrder route handler

--- API/api.js
+++ API/api.js
@@ -4,11 +4,20 @@ const Api = require('claudia-api-builder');
 const api = new Api();
 
 const getPizzas = require('./handlers/get-pizzas');
+const createOrder = require('./handlers/create-order');
 
-api.get('/', () => 'Welcome to Aunt Maria\'s Pizzeria');
+api.get('/', 
+        () => 'Welcome to Aunt Maria\'s Pizzeria');
 
-api.get('/pizzas', () => getPizzas());
+api.get('/pizzas', 
+        () => getPizzas());
 
-api.get('/pizzas/{id}', request => getPizzas(request.pathParams.id), { error: 404 });
+api.get('/pizzas/{id}', 
+        request => getPizzas(request.pathParams.id), 
+        { error: 404 });
+
+api.post('/orders', 
+         request => createOrder(request.body), 
+         { success: 201, error: 400 });
 
 module.exports = api;
