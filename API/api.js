edb78e86c4ef1c7a3873401c7be27659f4546769
Tijana <tijana.ilic87@protonmail.com>
2020-08-12 01:08:14 +0200

    Add GET pizzas/id route and update getPizzas route handler

--- API/api.js
+++ API/api.js
@@ -5,6 +5,10 @@ const api = new Api();
 
 const getPizzas = require('./handlers/get-pizzas');
 
+api.get('/', () => 'Welcome to Aunt Maria\'s Pizzeria');
+
 api.get('/pizzas', () => getPizzas());
+
+api.get('/pizzas/{id}', request => getPizzas(request.pathParams.id), { error: 404 });
 
 module.exports = api;
--- API/handlers/get-pizzas.js
+++ API/handlers/get-pizzas.js
@@ -2,6 +2,18 @@
 
 const pizzas = require('../data/pizzas.json');
 
-const getPizzas = () => pizzas;
+const getPizzas = (pizzaId) => {
+  if (!pizzaId) {
+    return pizzas;
+  }
+
+  const pizza = pizzas.find(pizza => pizza.id == pizzaId);
+
+  if (pizza) {
+    return pizza;
+  }
+
+  throw new Error('The pizza you requested was not found.');
+};
 
 module.exports = getPizzas;
