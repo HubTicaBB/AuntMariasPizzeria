df9455729f89cd25c6baacbd2a03fb883a51485a
Tijana <tijana.ilic87@protonmail.com>
2020-08-12 10:12:23 +0200

    Update PUT (updateOrder) to use DynamoDB

--- API/handlers/update-order.js
+++ API/handlers/update-order.js
@@ -1,11 +1,27 @@
 'use strict'
 
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+
 const updateOrder = (orderId, updates) => {
-  if (!orderId || !updates || !updates.pizzaId || !updates.address ) {
+  if (typeof orderId === 'undefined' || !updates || !updates.pizzaId || !updates.address ) {
     throw new Error('To update an order please provide new pizza type and address');
   }
 
-  return { message: `Order ${orderId} is successfully updated`};
+  return docClient.update({
+    TableName: 'pizza-orders',
+    Key: { orderId: orderId },
+    UpdateExpression: 'set pizzaId = :p, address = :a',
+    ExpressionAttributeValues: {
+      ':p' : updates.pizzaId,
+      ':a' : updates.address
+    },
+    ReturnValues: 'ALL_NEW'
+  }).promise()
+    .then(response => response.Attributes)
+    .catch(updateError => {
+      throw updateError;
+    });
 };
 
 module.exports = updateOrder;
