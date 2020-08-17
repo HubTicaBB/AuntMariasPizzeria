15f42a4e5a798be12613b8d2e21611bf5fd66b17
Tijana <tijana.ilic87@protonmail.com>
2020-08-17 19:11:38 +0200

    Implement https://some-like-it-hot.effortless-serverless.com/delivery API

--- API/api.js
+++ API/api.js
@@ -8,42 +8,44 @@ const getOrders = require('./handlers/get-orders');
 const createOrder = require('./handlers/create-order');
 const updateOrder = require('./handlers/update-order');
 const deleteOrder = require('./handlers/delete-order');
+const updateDeliveryStatus = require('./handlers/update-delivery-status');
 
 api.get('/', 
         () => 'Welcome to Aunt Maria\'s Pizzeria'
 );
 
 api.get('/pizzas', 
-        () => getPizzas()
-);
+        () => getPizzas());
 
 api.get('/pizzas/{id}', 
         request => getPizzas(request.pathParams.id), 
-        { error: 404 }
-);
+        { error: 404 });
+
+api.post('/pizzas',
+        request => createPizza(request.body),
+        { success: 201, error: 400 });
 
 api.get('/orders',
-        () => getOrders()
-);
+        () => getOrders());
 
 api.get('/orders/{id}',
         request => getOrders(request.pathParams.id),
-        { error: 404 }
-);
+        { error: 404 });
 
 api.post('/orders', 
         request => createOrder(request.body), 
-        { success: 201, error: 400 }
-);
+        { success: 201, error: 400 });
 
 api.put('/orders/{id}',
         request => updateOrder(request.pathParams.id, request.body),
-        { error: 400 }
-);
+        { error: 400 });
 
 api.delete('/orders/{id}',
         request => deleteOrder(request.pathParams.id),
-        { error: 400 }
-);
+        { success: 200, error: 400 });
+
+api.post('/delivery',
+        request => updateDeliveryStatus(request.body),
+        { success: 200, error: 400 });
 
 module.exports = api;
--- API/handlers/create-order.js
+++ API/handlers/create-order.js
@@ -2,26 +2,39 @@
 
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
-const uuid = require('uuid').v4;
+const minReqPromise = require('minimal-request-promise');
 
-const createOrder = order => {
+module.exports = function createOrder(order) {
   if (!order || !order.pizzaId || !order.address) {
     throw new Error('To order pizza please provide pizza type and address where pizza should be delivered.');
   }
 
-  return docClient.put({
-    TableName: 'pizza-orders',
-    Item: {
-      orderId: uuid(),
-      pizzaId: order.pizzaId,
-      address: order.address,
-      orderStatus: 'pending'
-    }
-  }).promise()
-    .then(response => response)
-    .catch(saveError => {
-      throw saveError;
-    });
-}
-
-module.exports = createOrder;
+  return minReqPromise.post('https://some-like-it-hot.effortless-serverless.com/delivery', { 
+    headers: {
+      "Authorization": "aunt-marias-pizzeria-1234456789",
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+      pickupTime: '15.34pm',
+      pickupAddress: 'Aunt Maria\'s Pizzeria',
+      deliveryAddress: order.address,
+      webhookUrl: 'https://r3emkux2e5.execute-api.us-east-1.amazonaws.com/latest/delivery'
+    })
+  })
+    .then(rawResponse => JSON.parse(rawResponse.body))
+    .then(response => {
+      return docClient.put({
+        TableName: 'pizza-orders',
+        Item: {
+          orderId: response.deliveryId,
+          pizzaId: order.pizzaId,
+          address: order.address,
+          orderStatus: 'pending',
+        }
+      }).promise()
+        .then(result => result)
+        .catch(saveError => {
+          throw saveError;
+        });
+    });  
+};
